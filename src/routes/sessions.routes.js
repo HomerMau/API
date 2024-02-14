b4007695f18a5f0643d38d6eefab78c7aab8d2f1
Tiago Lucas Da Silva <titi020604@gmail.com>
2024-02-14 16:05:01 -0300

    fix: typeerror

--- src/routes/sessions.routes.js
+++ src/routes/sessions.routes.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 
 const SessionsController = require("../controllers/SessionsController");
-const SessionsController = new SessionsController();
+const sessionsController = new SessionsController();
 
 const sessionsRoutes = Router();
 sessionsRoutes.post("/", sessionsController.create);
