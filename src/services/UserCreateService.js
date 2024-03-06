103d3d1ed0f11796885c6be98875dbbebd157008
Tiago Lucas Da Silva <titi020604@gmail.com>
2024-03-06 15:04:34 -0300

    feat: Unitary Tests

--- src/services/UserCreateService.js
+++ src/services/UserCreateService.js
@@ -1,4 +1,4 @@
-const { hash, compare } = require("bcryptjs")
+const { hash } = require("bcryptjs")
 
 const AppError = require("../utils/AppError")
 
@@ -16,7 +16,8 @@ class UserCreateService {
 
     const hashedPassword = await hash(password, 8)
 
-    await this.userRepository.create({ name, email, password: hashedPassword })
+    const userCreated = await this.userRepository.create({ name, email, password: hashedPassword })
+    return userCreated;
   }
 }
 
